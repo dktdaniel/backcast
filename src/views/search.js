2c106a2846144fe922b57b394e598244c2a2e1d6
Hack Reactor Student <student@hackreactor.com>
2017-09-06 20:55:52 -0700

    Significant progress on view rendering

--- src/views/search.js
+++ src/views/search.js
@@ -3,7 +3,13 @@ var SearchView = Backbone.View.extend({
 
   render: function() {
     this.$el.html(this.template());
-    return this;
+    return this.$el.html();
+  },
+  events: {
+    'click': 'aFunc'
+  },
+  aFunc: function() {
+    console.log('SEARCH BAR ENTERED!');
   },
 
   template: templateURL('src/templates/search.html')
--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,9 +1,25 @@
 var VideoListEntryView = Backbone.View.extend({
 
-
+  initialize: function() {
+    console.log(this.$el);
+  },
   render: function() {
-    this.$el.html(this.template(this.model.attributes));
-    return this;
+    this.$el.html(this.template(
+      this.model.attributes
+      // title: this.model.get('snippet').title, 
+      // description: this.model.get('snippet').description,
+      // url: this.model.get('snippet').thumbnails.default.url
+    ));
+    return this.$el.html();
+  },
+  events: {
+    'click div': 'handleClick',
+    'mouseenter div': 'handleClick',
+
+  },
+  handleClick: function() {
+    console.log('HEYYYYYYY!');
+    this.model.select();
   },
 
   template: templateURL('src/templates/videoListEntry.html')
